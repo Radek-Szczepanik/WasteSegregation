fadb29e37f9d5e72fe0c9bb53c3f2fa5e303ad97
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2022-07-07 22:17:53 +0200

    Add connection to data base

--- WasteSegregation/GlobalUsings.cs
+++ WasteSegregation/GlobalUsings.cs
@@ -4,6 +4,9 @@ global using Microsoft.AspNetCore.Builder;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using WasteSegregation.DataAccess.Repository;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using WasteSegregation.DataAccess;
 
 
 
