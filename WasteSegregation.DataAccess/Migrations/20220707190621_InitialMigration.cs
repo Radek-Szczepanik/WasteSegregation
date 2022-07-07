3000533abb5caaf798db851893e35aa485b0c58c
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2022-07-07 21:16:50 +0200

    Create data base

---
+++ WasteSegregation.DataAccess/Migrations/20220707190621_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WasteSegregation.DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WasteBags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    YellowBag = table.Column<int>(type: "int", maxLength: 50, nullable: false),
+                    GreenBag = table.Column<int>(type: "int", maxLength: 50, nullable: false),
+                    BlueBag = table.Column<int>(type: "int", maxLength: 50, nullable: false),
+                    BrownBag = table.Column<int>(type: "int", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WasteBags", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WasteBags");
+        }
+    }
+}
