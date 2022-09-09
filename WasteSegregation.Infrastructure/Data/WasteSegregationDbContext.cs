2096e807f3fb0f408c1db64013b00e1a32554cea
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2022-09-09 21:46:25 +0200

    Add DbContext

---
+++ WasteSegregation.Infrastructure/Data/WasteSegregationDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace WasteSegregation.Infrastructure.Data;
+
+public class WasteSegregationDbContext : DbContext
+{
+	public WasteSegregationDbContext(DbContextOptions<WasteSegregationDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<RealEstate> RealEstates { get; set; }
+	public DbSet<RealEstateWaste> RealEstateWastes { get; set; }
+}
