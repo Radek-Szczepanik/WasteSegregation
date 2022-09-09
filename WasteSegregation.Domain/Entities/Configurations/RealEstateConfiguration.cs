1dd29a1dbd133590d170690775217a0121186047
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2022-09-09 21:39:43 +0200

    Add configuration to RealEstate entity

---
+++ WasteSegregation.Domain/Entities/Configurations/RealEstateConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace WasteSegregation.Domain.Entities.Configurations;
+
+public class RealEstateConfiguration : IEntityTypeConfiguration<RealEstate>
+{
+    public void Configure(EntityTypeBuilder<RealEstate> builder)
+    {
+        builder.Property(x => x.Street)
+               .IsRequired()
+               .HasMaxLength(30);
+
+        builder.Property(x => x.StreetNumber)
+               .IsRequired()
+               .HasMaxLength(10);
+
+        builder.Property(x => x.PostCode)
+               .IsRequired()
+               .HasMaxLength(6);
+
+        builder.Property(x => x.City)
+               .IsRequired()
+               .HasMaxLength(20);
+
+        builder.HasMany(x => x.RealEstateWastes)
+               .WithOne(y => y.RealEstate)
+               .HasForeignKey(z => z.RealEstateId);
+    }
+}
--- WasteSegregation.Domain/GlobalUsings.cs
+++ WasteSegregation.Domain/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using WasteSegregation.Domain.Common;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
