d5765a31e91000ea1cfe2aecb9646ed067b46046
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2022-09-29 13:12:38 +0200

    Add user roles

--- WasteSegregation.WebAPI/Controllers/IdentityController.cs
+++ WasteSegregation.WebAPI/Controllers/IdentityController.cs
@@ -5,11 +5,15 @@
 public class IdentityController : ControllerBase
 {
     private readonly UserManager<ApplicationUser> userManager;
+    private readonly RoleManager<IdentityRole> roleManager;
     private readonly IConfiguration configuration;
 
-    public IdentityController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+    public IdentityController(UserManager<ApplicationUser> userManager,
+                              RoleManager<IdentityRole> roleManager,
+                              IConfiguration configuration)
     {
         this.userManager = userManager;
+        this.roleManager = roleManager;
         this.configuration = configuration;
     }
 
@@ -45,6 +49,51 @@ public class IdentityController : ControllerBase
             });
         }
 
+        if (await roleManager.RoleExistsAsync(UserRoles.User))
+            await roleManager.CreateAsync(new IdentityRole(UserRoles.User));
+
+        await userManager.AddToRoleAsync(user, UserRoles.User);
+
+        return Ok(new Response<bool> { Succeeded = true, Message = "User created successfully!" });
+    }
+
+    [HttpPost]
+    [Route("RegisterAdmin")]
+    public async Task<IActionResult> RegisterAdmin(RegisterModel registerModel)
+    {
+        var userExist = await userManager.FindByEmailAsync(registerModel.Email);
+        if (userExist != null)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new Response<bool>
+            {
+                Succeeded = false,
+                Message = "User already exists!"
+            });
+        }
+
+        ApplicationUser user = new ApplicationUser()
+        {
+            Email = registerModel.Email,
+            SecurityStamp = Guid.NewGuid().ToString(),
+            UserName = registerModel.Username
+        };
+
+        var result = await userManager.CreateAsync(user, registerModel.Password);
+        if (!result.Succeeded)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new Response<bool>
+            {
+                Succeeded = false,
+                Message = "User creation failed!",
+                Errors = result.Errors.Select(x => x.Description)
+            });
+        }
+
+        if (await roleManager.RoleExistsAsync(UserRoles.Admin))
+            await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
+
+        await userManager.AddToRoleAsync(user, UserRoles.Admin);
+
         return Ok(new Response<bool> { Succeeded = true, Message = "User created successfully!" });
     }
 
@@ -62,6 +111,12 @@ public class IdentityController : ControllerBase
                 new Claim(ClaimTypes.Email, login.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             };
+
+            var userRoles = await userManager.GetRolesAsync(user);
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
 
             var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
 
