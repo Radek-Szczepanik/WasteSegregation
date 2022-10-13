650be014a1f4f09ea3d06e64c6d2e96e81f70492
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2022-10-13 22:23:27 +0200

    Test GetByIdAsync method

--- UnitTests/Services/RealEstateServiceTests.cs
+++ UnitTests/Services/RealEstateServiceTests.cs
@@ -1,4 +1,6 @@
-﻿namespace UnitTests.Services;
+﻿using FluentAssertions;
+
+namespace UnitTests.Services;
 
 public class RealEstateServiceTests
 {
@@ -34,4 +36,49 @@ public class RealEstateServiceTests
         realEstateRepositoryMock.Verify(x => x.AddAsync(It.IsAny<RealEstate>()), Times.Once);
     }
 
+    [Fact]
+    public async Task when_invoking_get_real_estate_should_invoke_get_real_estate_repository()
+    {
+        // Arrange
+        var realEstateRepositoryMock = new Mock<IRealEstateRepository>();
+        var mapperMock = new Mock<IMapper>();
+
+        var realEstateService = new RealEstateService(realEstateRepositoryMock.Object, mapperMock.Object);
+
+        var realEstate = new RealEstate()
+        {
+            Id = 1,
+            Street = "Test street",
+            StreetNumber = "12345",
+            PostCode = "12-345",
+            City = "Test city"
+        };
+
+        var realEstateDto = new RealEstateDto()
+        {
+            Id = realEstate.Id,
+            Street = realEstate.Street,
+            StreetNumber = realEstate.StreetNumber,
+            PostCode = realEstate.PostCode,
+            City = realEstate.City,
+        };
+
+        mapperMock.Setup(x => x.Map<RealEstate>(realEstateDto)).Returns(realEstate);
+        realEstateRepositoryMock.Setup(x => x.GetByIdAsync(realEstate.Id)).ReturnsAsync(realEstate);
+
+        // Act
+        var existingRealEstateDto = await realEstateService.GetByIdAsync(realEstate.Id);
+
+        // Assert
+        realEstateRepositoryMock.Verify(x => x.GetByIdAsync(realEstate.Id), Times.Once);
+        realEstateDto.Should().NotBeNull();
+        realEstateDto.Street.Should().NotBeNull();
+        realEstateDto.StreetNumber.Should().NotBeNull();
+        realEstateDto.PostCode.Should().NotBeNull();
+        realEstateDto.City.Should().NotBeNull();
+        realEstateDto.Street.Should().BeEquivalentTo(realEstate.Street);
+        realEstateDto.StreetNumber.Should().BeEquivalentTo(realEstate.StreetNumber);
+        realEstateDto.PostCode.Should().BeEquivalentTo(realEstate.PostCode);
+        realEstateDto.City.Should().BeEquivalentTo(realEstate.City);
+    }
 }
