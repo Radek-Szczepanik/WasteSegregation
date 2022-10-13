4f743ea692f9a7776a04603f4597f79cc96f4635
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2022-10-13 22:04:04 +0200

    Test AddAsync method

---
+++ UnitTests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using Xunit;
+global using System.Threading.Tasks;
+global using WasteSegregation.Application.Services;
+global using AutoMapper;
+global using Moq;
+global using WasteSegregation.Domain.Interfaces;
+global using WasteSegregation.Application.Dto;
+global using WasteSegregation.Domain.Entities;
+
---
+++ UnitTests/Services/RealEstateServiceTests.cs
@@ -0,0 +1,37 @@
+﻿namespace UnitTests.Services;
+
+public class RealEstateServiceTests
+{
+    [Fact]
+    public async Task add_real_estate_should_invoke_add_real_estate_repository()
+    {
+        // Arrange
+        var realEstateRepositoryMock = new Mock<IRealEstateRepository>();
+        var mapperMock = new Mock<IMapper>();
+
+        var realEstateService = new RealEstateService(realEstateRepositoryMock.Object, mapperMock.Object);
+
+        var realEstateDto = new CreateRealEstateDto()
+        {
+            Street = "Test street",
+            StreetNumber = "12345",
+            PostCode = "12-345",
+            City = "Test city"
+        };
+
+        mapperMock.Setup(x => x.Map<RealEstate>(realEstateDto)).Returns(new RealEstate()
+        {
+            Street = realEstateDto.Street,
+            StreetNumber = realEstateDto.StreetNumber,
+            PostCode = realEstateDto.PostCode,
+            City = realEstateDto.City,
+        });
+
+        // Act
+        await realEstateService.AddAsync(realEstateDto, "c9351c9e-e763-4784-b9ca-66dd31e27419");
+
+        // Assert
+        realEstateRepositoryMock.Verify(x => x.AddAsync(It.IsAny<RealEstate>()), Times.Once);
+    }
+
+}
